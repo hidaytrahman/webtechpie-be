c5aea7dcf4bc07ca1ac04ae278e0759a9717911b
Hidayt Rahman <hidaytrahman@gmail.com>
2024-02-03 22:07:39 +0530

    chore: data and utils

---
+++ src/features/solutions/data.ts
@@ -0,0 +1,17 @@
+export const solutionHighlights = [
+	"Get trained by subject matter expert's",
+	"Live projects from scratch",
+	"Open discussions",
+	"Collborative teams",
+	"Mock Interviews",
+	"On demand training (Tech only)"
+]
+
+export const solutions = [
+	"Get trained by subject matter expert's",
+	"Live projects from scratch",
+	"Open discussions",
+	"Collborative teams",
+	"Mock Interviews",
+	"On demand training (Tech only)"
+]
