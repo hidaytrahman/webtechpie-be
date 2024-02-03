6ebb97bb9e5cbe9ad18606f19fb08ed90d824455
Hidayt Rahman <hidaytrahman@gmail.com>
2024-02-04 00:16:35 +0530

    feat: allow app origins

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1/');
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // "preflightContinue": false,
+    // "optionsSuccessStatus": 204
+  });
+
   await app.listen(8080);
 }
 bootstrap();
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,1 +1,3 @@
-export { }
+export const allowedOrigins = [
+	'http://localhost:3000'
+];
