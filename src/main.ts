d1dc36deda658163a277547b464c81415efd50ae
Rahul Anand <rahul.anand@eagleview.com>
2025-04-04 20:34:35 +0530

    Swagger Integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { allowedOrigins } from "./utils/data";
 import { ValidationPipe } from "@nestjs/common";
 import { AllExceptionsFilter } from "./config/all-exception.filter";
 import { APP_CONFIG } from "./config/app.config";
-
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
@@ -21,6 +21,20 @@ async function bootstrap() {
 
 	app.useGlobalPipes(new ValidationPipe({ transform: true }));
 	app.useGlobalFilters(new AllExceptionsFilter(app.getHttpAdapter()));
+
+	const config = new DocumentBuilder()
+		.setTitle('WebTechPie API')
+		.setDescription('The WebTechPie API documentation')
+		.setVersion('1.0')
+		.addTag('auth', 'Authentication endpoints')
+		.addTag('contact', 'Contact form endpoints')
+		.addTag('solutions', 'Solutions endpoints')
+		.addTag('teams', 'Teams endpoints')
+		.addTag('pages', 'Pages endpoints')
+		.addTag('plan', 'Plan endpoints')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
 
 	await app.listen(process.env.PORT || 8080);
 }
